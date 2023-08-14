ef7d6f12749becb862f893278121c10233a770a3
Aivansky <orenlascari@gmail.com>
2023-08-14 16:33:55 -0700

    8/14/23 re learning java class calls

---
+++ src/secndclass.java
@@ -0,0 +1,12 @@
+
+
+public class secndclass {
+    public static void overlay() {
+
+        System.out.printf("System working - printing class outside main\n\n");
+    }
+
+    public static void prompt(){
+        System.out.printf("2nd class outside main 2nd function\n\n");
+    }
+}
